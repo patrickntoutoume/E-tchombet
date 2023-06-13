f3612375b0830ece8c2a0784063d650c18b11508
patrickntoutoume <patrickntoutoume01@gmail.com>
2023-06-13 04:14:13 +0100

    first commit

---
+++ database/migrations/2023_06_12_235506_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->longText('description');
+            $table->foreign('description');
+            $table->string('cover');
+
+            $table->integer('current_stock');
+            $table->date('publish');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
